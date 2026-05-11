50b8d748ff5db945ca06c08f5f4f4546b5d3f88e
QAZYAE <nike200358@gmail.com>
2026-05-11 16:44:50 +0300

    Start-stop added

--- src/main_window.py
+++ src/main_window.py
@@ -27,9 +27,8 @@ class MainWindow(QMainWindow):
         # Check status
         self.btn_check_status.clicked.connect(self.check_yadisk_status)
         self.timer = QTimer(self)
-        self.timer.singleShot(0, self.check_yadisk_status)
         self.timer.timeout.connect(self.check_yadisk_status)
-        self.timer.start(1000)  # 1 second
+        self.start_timer()
         # File dialog
         self.btn_path.clicked.connect(self.open_file_dialog)
         # Showing main window
@@ -38,8 +37,65 @@ class MainWindow(QMainWindow):
         
     def check_yadisk_status(self):
         """Check current yandex disk status"""
-        res = subprocess.run(['yandex-disk', 'status'], capture_output=True)
-        self.label_yadisk_status.setText(res.stdout.decode())
+        try:
+            res = subprocess.run(['yandex-disk', 'status'], capture_output=True)
+            self.label_yadisk_status_full.setText(res.stdout.decode())
+            # TODO:
+            if res:
+                self.yadisk_status = True
+            else:
+                self.yadisk_status = False
+        except Exception as e:
+            self.label_yadisk_status_full.setText(f'{type(e).__name__}: {e}')
+            self.yadisk_status = False
+        if self.yadisk_status:
+            self.label_yadisk_status.setText('Daemon running')
+        else:
+            self.label_yadisk_status.setText('Daemon stopped')
+            
+            
+    def start_timer(self):
+        """Start the updating timer"""
+        self.check_yadisk_status()
+        self.timer.start(1000)  # 1 
+        
+        
+    def stop_timer(self):
+        """Stop the timer"""
+        self.timer.stop()
+        
+            
+    def start_stop_yadisk(self):
+        """Start or stop the daemon"""
+        if self.yadisk_status:  # Stopping
+            self.label_yadisk_status.setText('Stopping...')
+            self.label_yadisk_status.repaint()
+            try:
+                res = subprocess.run(['yandex-disk', 'stop'], capture_output=True)
+                if res.stdout.decode() == 'Daemon stopped':
+                    self.yadisk_status = False
+                    self.stop_timer()
+                else:
+                    raise RuntimeError(f'Could not stop the daemon. Std error: {res.stderr.decode()}')
+            except Exception as e:
+                self.yadisk_status = True
+                self.label_yadisk_status_full.setText(f'Error occured:\n{type(e).__name__}:\n{e}')
+                self.start_timer()
+        else:  # Starting
+            self.label_yadisk_status.setText('Starting...')
+            self.label_yadisk_status.repaint()
+            try:
+                res = subprocess.run(['yandex-disk', 'start'], capture_output=True)
+                if res.stdout.decode() == 'Daemon started':
+                    self.yadisk_status = True
+                    self.start_timer()
+                else:
+                    raise RuntimeError(f'Could not start the daemon. Std error: {res.stderr.decode()}')
+            except Exception as e:
+                self.yadisk_status = False
+                self.label_yadisk_status_full.setText(f'Error occured:\n{type(e).__name__}:\n{e}')
+                self.stop_timer()
+        
         
         
     def open_file_dialog(self):
